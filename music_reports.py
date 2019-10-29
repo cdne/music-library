e543f91c348823539afb6c2830d8cf081f7713a6
Dan Dumitriu <dandumitriu33@gmail.com>
2019-10-29 12:54:54 +0200

    music_reports skeleton

--- music_reports.py
+++ music_reports.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 
 # read file and place lines in dictionary
 
-def read_file():
+def read_file(): 
     d = {}
     with open ("file.txt", "r") as file:
             for line in file: 
@@ -13,10 +13,11 @@ def read_file():
     return d  
 
 
+
 # placeholder for files
 hold_lines = read_file()
 
-def sort_by_genre(temp_dict):
+def sort_by_genre(temp_dict): # requirement 2
     genre = []
     for key in temp_dict:
         genre.append([key, temp_dict[key][3]])
@@ -26,3 +27,57 @@ def sort_by_genre(temp_dict):
     for i in genre:
         print(f'{temp_dict[i[0]]}\n')
 
+# def time_range_album(): # requirement 3
+#     input('')
+#     return
+
+
+# def shortest(): # requireent 4
+#     return
+
+
+# def longest(): # requirement 4
+#     return
+
+
+# def artist_albums(): #requirement 5
+#     input('artist')
+#     return
+
+
+# def sort_by_album_name(): # requirement 6
+#     input('album name')
+#     return
+
+
+# def oldest_album(): # requirement 7
+#     return
+
+
+# def youngest_album(): # requirement 7
+#     return
+
+
+# def count_all_albums(): # requirement 7
+#     return
+
+
+# def how_many_given_genre(): # requirement 7
+#     return
+
+
+# def suggested_albums(): # requirement 8
+#     input('album name')
+#     # suggest albums with the same genre
+#     return
+
+
+# def add_new_album(): # requirement 9
+#     return
+
+
+# def edit_ablum(): # requirement 10
+#     return
+
+
+# def export_new_to_file(): # requirement 11
