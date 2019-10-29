67805d0769ea09542176b89ad4b6961c74bc2e44
Dan Dumitriu <dandumitriu33@gmail.com>
2019-10-29 14:26:12 +0200

    Added requirement 7.1 oldest album.

--- music_reports.py
+++ music_reports.py
@@ -73,8 +73,16 @@ print('Requested album is', shortest_longest('longest'))
 #     return
 
 
-# def oldest_album(): # requirement 7
-#     return
+def oldest_album():  # requirement 7.1 oldest album
+    dict_from_file = read_file()
+    album_years = []
+    for key in dict_from_file:
+        album_years.append([key, dict_from_file[key][2]])
+    album_years.sort(key=lambda x: int(x[1]))
+    return dict_from_file[album_years[0][0]]
+
+
+print('The oldest album is: ', oldest_album())
 
 
 # def youngest_album(): # requirement 7
