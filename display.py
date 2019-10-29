6b34c81b8ccef95c9744585754e1bf25d18777e8
Claudiu Duneata <cduneata@gmail.com>
2019-10-29 11:13:11 +0200

    added functions

---
+++ display.py
@@ -0,0 +1,8 @@
+from music_reports import *
+
+def print_unsorted():
+    print(read_file())
+    
+
+
+print_unsorted()
--- music_reports.py
+++ music_reports.py
@@ -1,12 +1,30 @@
 from collections import defaultdict
 
-d = defaultdict(list)
 
 
-with open ("text_albums_data.txt", "r") as file:
-        for line in file: 
-            items = line.split(",")
-            key, values = items[1], items[:]
-            d[str(key)] = values
+
+# read file and place lines in dictionary
+
+def read_file():
+    d = {}
+    with open ("file.txt", "r") as file:
+            for line in file: 
+                items = line.strip().split(',')
+                key, values = items[1], items[:]
+                d[str(key)] = values
+    
+    return d  
+
+
+g = read_file()
+
+def sort_by_genre(temp_dict):
+    genre = []
+    for key in temp_dict:
+        print(key)
+        # genre.append([values,key])
+        # genre.sort()
         
-print(d)
+
+
+sort_by_genre(g)
