8fa3175bb27d7a991e5fe4391c7b97a60e38dcdc
Dan Dumitriu <dandumitriu33@gmail.com>
2019-10-29 15:38:37 +0200

    Added the 10 requirement, edit album.

--- music_reports.py
+++ music_reports.py
@@ -1,29 +1,24 @@
 from collections import defaultdict
 
+# read file and place lines in a dictionary
 
-# read file and place lines in dictionary
 
-def read_file(): 
+def read_file():
     d = {}
-    with open ("file.txt", "r") as file:
-            for line in file: 
-                items = line.strip().split(',')
-                key, values = items[1], items[:]
-                d[str(key)] = values
-    
-    return d  
+    with open("file.txt", "r") as file:
+        for line in file:
+            items = line.strip().split(',')
+            key, values = items[1], items[:]
+            d[str(key)] = values
+    return d
 
 
-
-# placeholder for files
-hold_lines = read_file()
-
-def sort_by_genre(temp_dict): # requirement 2
+def sort_by_genre(temp_dict):  # requirement 2
     genre = []
     for key in temp_dict:
         genre.append([key, temp_dict[key][3]])
     print(genre)
-    genre.sort(key = lambda x: x[1])
+    genre.sort(key=lambda x: x[1])
     print('sorted by genre', genre)
     for i in genre:
         print(f'{temp_dict[i[0]]}\n')
@@ -58,8 +53,8 @@ def shortest_longest(what_album):  # requireent 4
         return dict_from_file[albums_list[-1][0]]
 
 
-print('Requested album is', shortest_longest('shortest'))
-print('Requested album is', shortest_longest('longest'))
+# print('Requested album is', shortest_longest('shortest'))
+# print('Requested album is', shortest_longest('longest'))
 
 
 
@@ -82,7 +77,7 @@ def oldest_album():  # requirement 7.1 oldest album
     return dict_from_file[album_years[0][0]]
 
 
-print('The oldest album is: ', oldest_album())
+# print('The oldest album is: ', oldest_album())
 
 
 def youngest_album():  # requirement 7.2 youngest album
@@ -94,7 +89,7 @@ def youngest_album():  # requirement 7.2 youngest album
     return dict_from_file[album_years[-1][0]]
 
 
-print('The youngest album is: ', youngest_album())
+# print('The youngest album is: ', youngest_album())
 
 
 def count_all_albums():  # requirement 7.3 total no. of albums
@@ -102,7 +97,7 @@ def count_all_albums():  # requirement 7.3 total no. of albums
     return len(dict_from_file)
 
 
-print('Total number of albums: ', count_all_albums())
+# print('Total number of albums: ', count_all_albums())
 
 
 def how_many_given_genre():  # requirement 7.4 how many albums based on genre
@@ -115,12 +110,12 @@ def how_many_given_genre():  # requirement 7.4 how many albums based on genre
         selected_genre = input('Please select the genre: ')
         if selected_genre in genres_list:
             valid_input = True
-        elif selected_genre == 'exit':
+        elif selected_genre == 'exit()':
             print('Thanks for using our product. Have a nice day!')
             exit()
         else:
             print('There are no albums of this genre in the music library.'
-                  ' Please select another genre or type "exit" to quit '
+                  ' Please select another genre or type "exit()" to quit '
                   'the program.')
     how_many_albums_by_genre = 0
     for key in dict_from_file:
@@ -129,7 +124,7 @@ def how_many_given_genre():  # requirement 7.4 how many albums based on genre
     return how_many_albums_by_genre
 
 
-print('Your selected genre returned ', how_many_given_genre())
+# print('Your selected genre returned ', how_many_given_genre())
 
 # def suggested_albums(): # requirement 8
 #     input('album name')
@@ -141,8 +136,40 @@ print('Your selected genre returned ', how_many_given_genre())
 #     return
 
 
-# def edit_ablum(): # requirement 10
-#     return
+def edit_album():  # requirement 10, needs mistake proof
+    dict_from_file = read_file()
+    valid_album_name = False
+    while valid_album_name is False:
+        album_to_edit = input('Please name the album you want to edit:')
+        if album_to_edit in dict_from_file:
+            valid_album_name = True
+        elif album_to_edit == 'exit()':
+            print('Thanks for using our product. Have a nice day!')
+            exit()
+        else:
+            print('There are no albums with this name in the music library.'
+                  ' Please select another album or type "exit()" to quit '
+                  'the program.')
+    edited_list = []
+    edited_album_name = input('Please enter new album name: ')
+    edited_album_artist = input('Please enter the artist: ')
+    edited_list.append(edited_album_artist)
+    edited_list.append(edited_album_name)
+    edited_album_year = input('Please enter the album year: ')
+    edited_list.append(edited_album_year)
+    edited_album_genre = input('Please enter the album genre: ')
+    edited_list.append(edited_album_genre)
+    edited_album_length = input('Please enter the album length: ')
+    edited_list.append(edited_album_length)
+    dict_from_file[edited_album_name] = dict_from_file.pop(album_to_edit)
+    dict_from_file[edited_album_name] = edited_list
+    print('Please be careful, the edited album is only saved in this session.'
+          ' If you would like to keep teh album in the list for longer, '
+          ' please consider exporting the current session library.')
+    print(dict_from_file)
+    # return
 
+
+edit_album()
 
 # def export_new_to_file(): # requirement 11
