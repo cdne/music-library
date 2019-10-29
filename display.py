f88f824b6720f6c576d4e2da63a592303edb7eab
Claudiu Duneata <cduneata@gmail.com>
2019-10-29 14:33:46 +0200

    Added 2 print functions

--- display.py
+++ display.py
@@ -1,9 +1,9 @@
-from music_reports_d import *
+from music_reports import *
 
 def print_unsorted():
-
-    print(sort_by_genre(hold_lines))
     
+    print(artist_albums(hold_lines))
+    print(sort_by_album_name(hold_lines))
  
 print_unsorted()
 
